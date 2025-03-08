01c9b14aad73fb9d3d1a5a92d4afb5cc721129c8
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2025-03-08 07:39:30 -0700

    creates routes for user and pokemon

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get '/users', to: 'users#index'
+  get '/user/:id', to: 'users#show'
+  get '/pokemon', to: 'pokemon#index'
+  get '/pokemon/:id', to: 'pokemon#show'
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  post '/pokemon', to: 'pokemon#create'
+  post '/user', to: 'users#create'
 end
